aa8e44bb186273e98b149a6f5e687d7b6bc48259
mmcguire91 <mmcguire91@gmail.com>
2020-07-18 12:02:28 -0400

    Refactored code to make task listview dynamic

--- lib/Components/TaskList.dart
+++ lib/Components/TaskList.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
 import 'TaskTile.dart';
 //This is the UI component and Logic that the task list will consist of within the white container at the bottom of the tasksview
+import '../Models/Task.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name: 'task'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        TaskTile(),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return TaskTile(
+            taskString: tasks[index].name,
+            isChecked: tasks[index].isDone,
+            /*return a task tile where
+        * taskString is retrieved from the TaskTile property, this retrieves the contents of the taskString
+        *  --> retrieves the tasks from the list of tasks,
+        *  --> specifically the name property from class Task in Task.dart
+        * isChecked is retrieved from the TaskTile property, this retrieves whether the checkbox is checked
+        *  --> retrieves the tasks from the list of tasks,
+        *  --> specifically the isDone property from class Task in Task.dart*/
+            checkboxCallback: (checkboxState) {
+              setState(() {
+                tasks[index].toggleDone();
+              });
+            }
+            //estbalish the logic (an anonymous function) for the checkbox to be checked or not,
+            );
+      },
+      itemCount: tasks.length, // display as many tasks as are in the list
     );
   }
 }
--- lib/Components/TaskTile.dart
+++ lib/Components/TaskTile.dart
@@ -1,56 +1,38 @@
 import 'package:flutter/material.dart';
 //This is the logic that will define the functionality of adding a task to the TaskList
 
-class TaskTile extends StatefulWidget {
-  TaskTile({this.task});
-
-  final String task;
-
-  @override
-  _TaskTileState createState() => _TaskTileState();
-}
-
-class _TaskTileState extends State<TaskTile> {
-  bool isChecked = false;
-
-  void checkboxCallback(bool checkboxState) {
-    setState(() {
-      isChecked = checkboxState;
-    });
-  }
-  //estbalish the logic (a function) for the checkbox to be checked or not
+class TaskTile extends StatelessWidget {
+  TaskTile({this.isChecked, this.taskString, this.checkboxCallback});
+  final bool isChecked;
+  final String taskString;
+  final Function checkboxCallback;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
-        '${widget.task}',
+        '$taskString',
         style: TextStyle(
           decoration: isChecked ? TextDecoration.lineThrough : null,
           //if the checkbox is checked then strikethrough the text
         ),
       ),
-      trailing: TaskCheckbox(
-        checkboxState:
-            isChecked, //checkboxState is equal to the variable of isChecked
-        toggleCheckbox:
-            checkboxCallback, //toggle checkbox on/off = logic of void function checkboxCallback
+      trailing: Checkbox(
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,
+        onChanged: checkboxCallback,
+        //toggle checkbox on/off = logic of void function checkboxCallback
       ),
     );
   }
 }
 
-class TaskCheckbox extends StatelessWidget {
-  TaskCheckbox({this.checkboxState, this.toggleCheckbox});
-  final bool checkboxState;
-  final Function toggleCheckbox;
-
-  @override
-  Widget build(BuildContext context) {
-    return Checkbox(
-      activeColor: Colors.lightBlueAccent,
-      value: checkboxState,
-      onChanged: toggleCheckbox,
-    );
-  }
-}
+//class TaskCheckbox extends StatelessWidget {
+//  TaskCheckbox({this.checkboxState, this.toggleCheckbox});
+//  final bool checkboxState;
+//  final Function toggleCheckbox;
+//
+//  @override
+//  Widget build(BuildContext context) {
+//  }
+//}
